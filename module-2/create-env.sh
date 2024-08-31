78a0b0eb31ae3e3ca404da88dbedd74f8404d06d
Michael Miller <mmiller29@hawk.iit.edu>
2024-08-31 01:59:27 -0400

    fixed filter

--- module-2/create-env.sh
+++ module-2/create-env.sh
@@ -32,7 +32,7 @@ echo "Waiting until instances are in RUNNING state..."
 
 # Collect your running instance IDS
 # https://stackoverflow.com/questions/31744316/aws-cli-filter-or-logic
-INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' -- filter "Name=instance-state-name,Values=running,pending")
+INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' --filter "Name=instance-state-name,Values=running,pending")
 
 echo $INSTANCEIDS
 
