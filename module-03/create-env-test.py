cc062e04fadaf510f67590c21f84c6ae7ff468b0
Michael Miller <mmiller29@hawk.iit.edu>
2024-08-31 18:32:13 -0400

    updated code

--- module-03/create-env-test.py
+++ module-03/create-env-test.py
@@ -7,26 +7,24 @@ import datetime
 import time
 from tqdm import tqdm
 
-###############################################################################
-# This file is used to assess the output of the create-env.sh file
-# DO NOT MODIFY this file!!!!
-# But you are welcome to inspect it
-###############################################################################
-
 # Assignment grand total
 grandtotal = 0
-totalPoints = 4
-assessmentName = "module-2-assessment-create-script"
+totalPoints = 6
+assessmentName = "module-3-create-assessment"
 correctNumberOfEC2Instances = 3
-tag = "module2-tag"
+correctNumberOfTargetGroups = 1
+correctNumberOfELBs         = 1
+tag = "module3-tag"
 
 # Documentation Links
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elbv2.html
 
 clientec2 = boto3.client('ec2')
+clientelbv2 = boto3.client('elbv2')
 
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_instances.html
-response = clientec2.describe_instances(
+responseEC2 = clientec2.describe_instances(
  Filters=[
      {
          'Name': 'instance-state-name',
@@ -35,7 +33,15 @@ response = clientec2.describe_instances(
 ],
 ) # End of function
 
-print("Begin tests for create-env.sh module 2...")
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elbv2/client/describe_load_balancers.html
+responseELB = clientelbv2.describe_load_balancers()
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elbv2/client/describe_target_groups.html
+responseTG = clientelbv2.describe_target_groups()
+
+##############################################################################
+print("Begin tests for create-env.sh module 3..")
+##############################################################################
 
 ##############################################################################
 # Check to make sure there are 3 instances of 'running' state
@@ -43,41 +49,41 @@ print("Begin tests for create-env.sh module 2...")
 print('*' * 79)
 print("Testing for the correct number of EC2 instances launched and in the RUNNING state...")
 try:
-  if len(response['Reservations'][0]['Instances']) == 0:
+  if len(responseEC2['Reservations'][0]['Instances']) == 0:
     print("Beginning tests...")
 except IndexError:  
   sys.exit("No EC2 instances in the RUNNING STATE - check that you ran your create-env.sh or wait 30-60 seconds more to make sure your instances are in the running state.")
 
-if len(response['Reservations'][0]['Instances']) == correctNumberOfEC2Instances:
-    print("Correct number of EC2 instances created, expecting " + str(correctNumberOfEC2Instances) + ", received " + str(len(response['Reservations'][0]['Instances'])) + ".")
-    for n in range(0,len(response['Reservations'][0]['Instances'])):
-      print("InstanceID of: " + response['Reservations'][0]['Instances'][n]['InstanceId'])
+if len(responseEC2['Reservations'][0]['Instances']) == correctNumberOfEC2Instances:
+    print("Correct number of EC2 instances created, expecting " + str(correctNumberOfEC2Instances) + ", received " + str(len(responseEC2['Reservations'][0]['Instances'])) + ".")
+    for n in range(0,len(responseEC2['Reservations'][0]['Instances'])):
+      print("InstanceID of: " + responseEC2['Reservations'][0]['Instances'][n]['InstanceId'])
     grandtotal += 1
 else:
-    print("Incorrect Number of EC2 instances created, expecting " + str(correctNumberOfEC2Instances) + ", received " + str(len(response['Reservations'][0]['Instances'])) + ".")
-    for n in range(0,len(response['Reservations'][0]['Instances'])):
-      print("InstanceID of: " + response['Reservations'][0]['Instances'][n]['InstanceId'])
+    print("Incorrect Number of EC2 instances created, expecting " + str(correctNumberOfEC2Instances) + ", received " + str(len(responseEC2['Reservations'][0]['Instances'])) + ".")
+    for n in range(0,len(responseEC2['Reservations'][0]['Instances'])):
+      print("InstanceID of: " + responseEC2['Reservations'][0]['Instances'][n]['InstanceId'])
 
 print('*' * 79)
 print("\r")
 ##############################################################################
-# Check Tag values to be 'module2-tag'
+# Check Tag values to be 'module3-tag'
 ##############################################################################
 print('*' * 79)
-print("Testing to make sure the running EC2 instances all have the tag of " + tag + "...")
+print("Testing to make sure the running EC2 instances all have the tag of 'module1-tag'...")
 checkTagTypeMismatch = False
-for n in range(0,len(response['Reservations'][0]['Instances'])):
-  if response['Reservations'][0]['Instances'][n]['Tags'][0]['Value'] == tag:
-    print("InstanceID of: " + response['Reservations'][0]['Instances'][n]['InstanceId'] + " has a tag of: " + response['Reservations'][0]['Instances'][n]['Tags'][0]['Value'])
+for n in range(0,len(responseEC2['Reservations'][0]['Instances'])):
+  if responseEC2['Reservations'][0]['Instances'][n]['Tags'][0]['Value'] == tag:
+    print("InstanceID of: " + responseEC2['Reservations'][0]['Instances'][n]['InstanceId'] + " has a tag of: " + responseEC2['Reservations'][0]['Instances'][n]['Tags'][0]['Value'])
   else:
      checkTagTypeMismatch = True
-     print("Incorrect tag set for Ec2 Instance: " + response['Reservations'][0]['Instances'][n]['InstanceId'] + ". Tag of: " + str(response['Reservations'][0]['Instances'][n]['Tags'][0]['Value']) + " set...")
+     print("Incorrect tag set for Ec2 Instance. Tag of: " + str(responseEC2['Reservations'][0]['Instances'][n]['Tags'][0]['Value']) + " set...")
 
-if checkTagTypeMismatch is False:
-   print("Correct. All Ec2 Instances have the correct tag of " + tag + "...")
+if checkTagTypeMismatch == False:
+   print("Correct. All Ec2 Instances have the correct tag of: " + tag + "...")
    grandtotal += 1
 else:
-   print("Incorrect. Some Ec2 Instances have the incorrect tag. Go back to your arguments.txt file and take a look at the value for $7.")
+   print("Incorrect. Some Ec2 Instances have the incorrect tag. Go back to your arguments.txt file and take a look at the value for $8.")
 
 print('*' * 79)
 print("\r")
@@ -87,15 +93,15 @@ print("\r")
 print('*' * 79)
 print("Testing to make sure the running EC2 instances are all of type t2.micro...")
 checkInstanceTypeMismatch = False
-for n in range(0,len(response['Reservations'][0]['Instances'])):
-  if response['Reservations'][0]['Instances'][n]['InstanceType'] == "t2.micro":
-    print("InstanceID of: " + response['Reservations'][0]['Instances'][n]['InstanceId'] + " and of InstanceType: " + response['Reservations'][0]['Instances'][n]['InstanceType'])
+for n in range(0,len(responseEC2['Reservations'][0]['Instances'])):
+  if responseEC2['Reservations'][0]['Instances'][n]['InstanceType'] == "t2.micro":
+    print("InstanceID of: " + responseEC2['Reservations'][0]['Instances'][n]['InstanceId'] + " and of InstanceType: " + responseEC2['Reservations'][0]['Instances'][n]['InstanceType'])
   else:
-     checkInstanceTypeMismatch = True
-     print("Incorrect Ec2 Instance Type of " + str(response['Reservations'][0]['Instances'][n]['InstanceType']) + " set.")
+     checkInstanceTypeMatch = True
+     print("Incorrect Ec2 Instance Type of " + str(responseEC2['Reservations'][0]['Instances'][n]['InstanceType']) + " set.")
 
-if checkInstanceTypeMismatch is False:
-   print("Correct Ec2 Instance Type launched for all EC2 instances.")
+if checkInstanceTypeMismatch == False:
+   print("Correct Ec2 Instance Type launched for all 3 EC2 instances.")
    grandtotal += 1
 else:
    print("Incorrect Ec2 Instance Types launched. Perhaps go back and take a look at the value in the arguments.txt $1 and $2.")
@@ -106,32 +112,65 @@ print("\r")
 # Check PublicDNS and HTTP return status to check if webserver was installed and working
 ##############################################################################
 print('*' * 79)
-print("Testing for the correct HTTP status (200) response from the webserver on the EC2 instances...")
+print("Testing for the correct HTTP status (200) response from the webserver via the ELB URL...")
 # https://pypi.org/project/tqdm/
 for i in tqdm(range(30)):
   time.sleep(1)
 
 checkHttpReturnStatusMismatch = False
-for n in range(0,len(response['Reservations'][0]['Instances'])):
-  print("http://" + response['Reservations'][0]['Instances'][n]['PublicDnsName'])
-  try:
-    res=requests.get("http://" + response['Reservations'][0]['Instances'][n]['PublicDnsName'], timeout=(3.05, 27))
-    if res.status_code == 200:
-      print("Successful request of the index.html file from " + str(response['Reservations'][0]['Instances'][n]['PublicDnsName']))
-    else:
-      checkHttpReturnStatusMismatch = True
-      print("Incorrect http response code: " + str(res.status_code) + "from " + str(response['Reservations'][0]['Instances'][n]['PublicDnsName']))
-  except requests.exceptions.ConnectionError as errc:
-    print ("Error Connecting:",errc)
+print("http://" + responseELB['LoadBalancers'][0]['DNSName'])
+try:
+  res=requests.get("http://" + responseELB['LoadBalancers'][0]['DNSName'])
+  if res.status_code == 200:
+    print("Successful request of the index.html file from " + str(responseELB['LoadBalancers'][0]['DNSName']))
+  else:
     checkHttpReturnStatusMismatch = True
-    print("No response code returned - not able to connect: " + str(res.status_code) + "from " + str(response['Reservations'][0]['Instances'][n]['PublicDnsName'])) 
-    sys.exit("Perhaps wait 1-2 minutes to let the install-env.sh server to finish installing the Nginx server and get the service ready to except external connections? Rerun this test script.")
+    print("Incorrect http response code: " + str(res.status_code) + "from " + str(responseELB['LoadBalancers'][0]['DNSName']))
+except requests.exceptions.ConnectionError as errc:
+  print ("Error Connecting:",errc)
+  checkHttpReturnStatusMismatch = True
+  print("No response code returned - not able to connect: " + str(res.status_code) + "from " + str(responseELB['LoadBalancers'][0]['DNSName'])) 
+  sys.exit("Perhaps wait 1-2 minutes to let the install-env.sh server to finish installing the Nginx server and get the service ready to except external connections? Rerun this test script.")
 
 if checkHttpReturnStatusMismatch == False:
    print("Correct HTTP status code of 200, received for all 3 EC2 instances.")
    grandtotal += 1
 else:
    print("Incorrect status code received. Perhaps go back and take a look at the --user-file value and the content of the install-env.sh script. Or check your security group to make sure the correct ports are open.")
+
+print('*' * 79)
+print("\r")
+##############################################################################
+# Check to see if Target Group is present
+##############################################################################
+print('*' * 79)
+print("Testing to make sure that 1 Target Group is present...")
+checkOneTargetGroupPresent = False
+if len(responseTG['TargetGroups']) > 1:
+  print("Expecting 1 target group, received " + str(len(responseTG['TargetGroups'])) + "...")
+  print("Perhaps double check that your destroy-env.sh scripts from previous assessments ran correctly or that your create-env.sh ran without error...")
+else:
+  checkOneTargetGroupPresent = True
+  print("Expecting 1 target group and received 1 target group: ")
+  print(str(responseTG['TargetGroups'][0]['TargetGroupArn']))
+  grandtotal += 1
+
+print('*' * 79)
+print("\r")
+##############################################################################
+# Check to see Load Balancer is attached to Target Group
+##############################################################################
+print('*' * 79)
+print("Testing to make sure that 1 Load Balancer is attached to the Target Group...")
+checkOneTargetGroupPresent = False
+if len(responseTG['TargetGroups'][0]['LoadBalancerArns']) == 0:
+  print("Expecting 1 LoadBalancerArn attached to target group, received " + str(len(responseTG['TargetGroups'][0]['LoadBalancerArns'])) + "...")
+  print("Perhaps double check that your destroy-env.sh scripts from previous assessments ran correctly or that your create-env.sh ran without error to attach your target-group to your ELB...")
+else:
+  checkOneTargetGroupPresent = True
+  print("Expecting 1 target group and recieved 1 load balancer arn attached to Target Group: ")
+  print(str(responseTG['TargetGroups'][0]['LoadBalancerArns']))
+  grandtotal += 1
 
 print('*' * 79)
 print("\r")
@@ -143,7 +182,7 @@ print("Your result is: " + str(grandtotal) + " out of " + str(totalPoints) + " p
 
 # Write results to a text file for import to the grade system
 # https://www.geeksforgeeks.org/sha-in-python/
-f = open('create-env-module-02-results.txt', 'w', encoding="utf-8")
+f = open('create-env-module-03-results.txt', 'w', encoding="utf-8")
 
 # Gather sha256 of module-name and grandtotal
 # https://stackoverflow.com/questions/70498432/how-to-hash-a-string-in-python
@@ -164,6 +203,6 @@ resultsdict = {
 print("Writing assessment grade to text file.")
 json.dump(resultsdict,f)
 print("Write successful! Ready to submit your Assessment.")
-print("You should now see a create-env-module-02-results.txt file has been generated.")
+print("You should now see a create-env-module-03-results.txt file has been generated.")
 f.close
 print('*' * 79)
