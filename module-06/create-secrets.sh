eccdea278861db1f373730c10a57167f6e1adc80
Michael Miller <mmiller29@hawk.iit.edu>
2024-09-29 17:37:57 -0400

    fix

--- module-06/create-secrets.sh
+++ module-06/create-secrets.sh
@@ -6,7 +6,7 @@ echo "Creating AWS secret..."
 # Modify your maria.json 
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/secretsmanager/get-secret-value.html#examples
-aws secretsmanager create-secret --name ${21} --secret-string file://maria.json
+aws secretsmanager create-secret --name ${21} --secret-string "{\"user\":\"michael\",\"password\":\"!Flames1983\"}"
 
 SECRET_ID=$(aws secretsmanager list-secrets --filters Key=name,Values=${21} --query 'SecretList[*].ARN')
 USERVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | sed 's/[\\nt]//g' | sed 's/^"//g' | sed 's/"$//g' | jq '.user' | sed 's/"//g')
